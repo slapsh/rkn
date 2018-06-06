7a256bb9b0d9f6184147a7d48dfdecce8b155030
slapsh <stanislav.lapshansky@gmail.com>
2018-06-06 15:58:22 +0300

    Change some filenames

--- makeRoute.sh
+++ makeRoute.sh
@@ -15,14 +15,14 @@ then
   diff -U 0 routes.txt routes-new.txt | sed '1,2d' | grep '^-' | cut -c2- > routes-del.txt
   diff -U 0 routes.txt routes-new.txt | sed '1,2d' | grep '^+' | cut -c2- > routes-add.txt
   mv routes-new.txt routes.txt
-  cat routes-del.txt | awk '{ print "/ip route remove [ find distance=1 dst-address=" $1 " gateway=ovpn-out1 ]" }' > vpn-routes.rsc
-  cat routes-add.txt | awk '{ print "/ip route add distance=1 dst-address=" $1 " gateway=ovpn-out1" }' >> vpn-routes.rsc
+  cat routes-del.txt | awk '{ print "/ip route remove [ find distance=1 dst-address=" $1 " gateway=ovpn-rkn ]" }' > vpn-routes-diff.rsc
+  cat routes-add.txt | awk '{ print "/ip route add distance=1 dst-address=" $1 " gateway=ovpn-rkn" }' >> vpn-routes-diff.rsc
   echo -n '+'
   cat routes-add.txt | wc -l
   echo -n '-'
   cat routes-del.txt | wc -l
 else
   cat net.txt ip.txt ip24.txt | ./supernets.py > routes.txt
-  echo "/ip route remove [ find gateway=ovpn-out1 static=yes ]" > vpn-routes.rsc
-  cat routes.txt | awk '{ print "/ip route add distance=1 dst-address=" $1 " gateway=ovpn-out1" }' >> vpn-routes.rsc
+  echo "/ip route remove [ find gateway=ovpn-rkn static=yes ]" > vpn-routes-full.rsc
+  cat routes.txt | awk '{ print "/ip route add distance=1 dst-address=" $1 " gateway=ovpn-rkn" }' >> vpn-routes-full.rsc
 fi
